12c8ee08f9710f22bb896727d8c2ae902495b4b7
Aryan Arora <aryanarora.w1@gmail.com>
2023-11-29 21:14:50 +0530

    chore: splash screen & lock to portrait & internet permission

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movieflix/di/locator.dart';
 import 'package:movieflix/presentation/homepage.dart';
@@ -8,6 +9,10 @@ import 'package:movieflix/presentation/movie_detail_page_viewmodel.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   await dotenv.load(fileName: ".env");
   setupLocator();
   runApp(const MovieFlix());
